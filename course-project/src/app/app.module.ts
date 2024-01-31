4e1d51f27d6ba44f3ead9f95d7cca3f8b2b2c937
Miguel Abreu <migabreu12@gmail.com>
2024-01-31 12:21:31 -0500

    Implemented the first pass at creating the shopping list components.

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './components/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './components/recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './components/recipe-book/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './components/recipe-book/recipe-detail/recipe-detail.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
