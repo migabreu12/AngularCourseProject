2bcf25a8e607aeca05351e604acda7ba98f82303
Miguel Abreu <migabreu12@gmail.com>
2024-02-08 14:48:01 -0500

    105. Implemented the example. Its much cleaner and I added a way to set the initial value of the isOpen property. You can use two decorators at once.

--- course-project/src/app/shared/dropdown.directive.ts
+++ course-project/src/app/shared/dropdown.directive.ts
@@ -1,37 +1,20 @@
-import { Directive, ElementRef, HostListener, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective implements OnInit {
-  @Input() dropdownClosed: boolean;
+  @Input() @HostBinding('class.open') isOpen: boolean;
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
+  constructor() { }
 
   public ngOnInit(): void {
-    if(!!!this.dropdownClosed) {
-      this.dropdownClosed = true;
+    if(!!!this.isOpen) {
+      this.isOpen = false;
     }
-
-    if(!this.dropdownClosed) {
-      this.toggleDropdownClass();
-    }
-;  }
+  }
 
   @HostListener('click') click() {
-    this.toggleDropdownClass();
-    this.toggleDropdownClosed();
-  }
-
-  private toggleDropdownClosed() {
-    this.dropdownClosed = !this.dropdownClosed;
-  }
-
-  private toggleDropdownClass() {
-    if(this.dropdownClosed) {
-      this.renderer.addClass(this.elementRef.nativeElement, 'open');
-    } else {
-      this.renderer.removeClass(this.elementRef.nativeElement, 'open');
-    }
+    this.isOpen = !this.isOpen;
   }
 }
