92079345facd7ece2bf11269dfedf1a6883a68f8
Miguel Abreu <migabreu12@gmail.com>
2024-02-08 14:54:16 -0500

    106. Updated code to allow for closing when click on anywhere.

--- course-project/src/app/shared/dropdown.directive.ts
+++ course-project/src/app/shared/dropdown.directive.ts
@@ -1,12 +1,13 @@
-import { Directive, HostBinding, HostListener, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective implements OnInit {
+  // the @HostBinding will automatically add the .open class whenever the isOpen property is set to true.
   @Input() @HostBinding('class.open') isOpen: boolean;
 
-  constructor() { }
+  constructor(private elementRef: ElementRef) { }
 
   public ngOnInit(): void {
     if(!!!this.isOpen) {
@@ -14,7 +15,9 @@ export class DropdownDirective implements OnInit {
     }
   }
 
-  @HostListener('click') click() {
-    this.isOpen = !this.isOpen;
+  // This allows the dropdown to close whenever a click is done outside of the dropdown.
+  // It also allows the dropdown to open correctly when toggling between dropdowns
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elementRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
 }
