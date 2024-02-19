ea51a3f8705b3b6361948a1f1ac5d35e499b91b2
Miguel Abreu <migabreu12@gmail.com>
2024-02-19 13:20:17 -0500

    Revert "Created a .net api to interface with the db. Added the models for tehe recipe and ingredient."
    This reverts commit bb5ec1dea15ce0948ea75d3c4f58f57e38420c28.

--- courseProjectApi/Models/Recipe.cs
+++
@@ -1,7 +0,0 @@
-namespace courseProjectApi.Model;
-
-public class Recipe {
-    public string RecipeId { get; set; } = default!;
-    public string? Name { get; set; }
-    public string? Description { get; set; }
-}
