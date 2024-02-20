bc5aea07aaa3e5635df3728e21cb16d9f0b5fed3
Miguel Abreu <migabreu12@gmail.com>
2024-02-20 12:14:19 -0500

    Added the ingredient primary key property attribute for consistency.

--- CourseProjectApi/Models/Ingredient.cs
+++ CourseProjectApi/Models/Ingredient.cs
@@ -1,6 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CourseProjectApi.Models;
 
 public class Ingredient {
+    [Key]
     public Guid IngredientId { get; set; }
     public string? Name { get; set; }
     public decimal? Amount { get; set; }
