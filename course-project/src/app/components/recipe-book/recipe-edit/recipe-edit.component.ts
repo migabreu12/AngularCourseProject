750665db4f06692a29183c259401a3ba7be15f78
Miguel Abreu <migabreu12@gmail.com>
2024-02-29 09:14:08 -0500

    169. Added logic to determine if the edit component should be in edit mode or new mode.

--- course-project/src/app/components/recipe-book/recipe-edit/recipe-edit.component.ts
+++ course-project/src/app/components/recipe-book/recipe-edit/recipe-edit.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
 })
-export class RecipeEditComponent {
-  
+export class RecipeEditComponent implements OnInit {
+  private id: number;
+  public isEditMode: boolean;
+
+  public constructor(private route: ActivatedRoute) {
+  }
+
+  public ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.id = +params["id"];
+      this.isEditMode = params["id"] != null;
+    });
+  }
 }
