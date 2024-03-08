99c607e84cd54309b1f1baa9ef7c4d09a19a1782
Miguel Abreu <migabreu12@gmail.com>
2024-03-08 15:08:33 -0500

    Implemented the initForm method and assigned the form the correct values based on edit mode or not. Synced the code behind form with the front end.

--- course-project/src/app/components/recipe-book/recipe-edit/recipe-edit.component.ts
+++ course-project/src/app/components/recipe-book/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { RecipeService } from 'src/app/services/recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -8,14 +10,37 @@ import { ActivatedRoute } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   private id: number;
   public isEditMode: boolean;
+  public recipeForm: FormGroup;
 
-  public constructor(private route: ActivatedRoute) {
+  public constructor(
+      private route: ActivatedRoute,
+      private recipeService: RecipeService
+    ) {
   }
 
   public ngOnInit(): void {
     this.route.params.subscribe((params) => {
       this.id = +params["id"];
       this.isEditMode = params["id"] != null;
+
+      this.initForm();
     });
+  }
+
+  private initForm() {
+    if(this.isEditMode) {
+      const recipeToEdit = this.recipeService.getRecipe(this.id);
+      this.recipeForm = new FormGroup({
+        "name": new FormControl(recipeToEdit.name, Validators.required),
+        "description": new FormControl(recipeToEdit.description),
+        "imagePath": new FormControl(recipeToEdit.imagePath, Validators.required)
+      });
+    } else {
+      this.recipeForm = new FormGroup({
+        "name": new FormControl(null, Validators.required),
+        "description": new FormControl(null),
+        "imagePath": new FormControl(null, Validators.required)
+      });
+    }
   }
 }
